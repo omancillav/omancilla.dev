d629fd26c29b3dfe2a74f9f45ecec9b817d7348f
omancillav <edgarmncllav04@gmail.com>
2025-10-16 10:59:52 -0600

    Refactor project structure: create 'projects.ts' and 'tags.ts' for better organization; update Container, Footer, and Projects components for improved layout and styling

---
+++ src/lib/projects.ts
@@ -0,0 +1,31 @@
+import { TAGS } from './tags'
+
+export const PROJECTS = [
+  {
+    title: 'Vet Sync - Gestión de citas veterinarias para mascotas',
+    description:
+      'Vet Sync es una aplicación web desarrollada con React y Tailwind CSS para gestionar clínicas veterinarias. Permite agendar citas, administrar expedientes de mascotas y controlar accesos por roles con una interfaz moderna y responsive.',
+    link: 'https://vetsync.vercel.app',
+    github: 'https://github.com/omancillav/vet-sync-app',
+    image: 'vetsync.webp',
+    tags: [TAGS.REACT, TAGS.TAILWIND, TAGS.SHADCN, TAGS.SUPABASE, TAGS.NODE, TAGS.EXPRESS, TAGS.JWT]
+  },
+  {
+    title: 'Cutfly - Acortador de URLs Open Source',
+    description:
+      'Acortador de URLs gratuito y de código abierto construido con Next.js y TypeScript. Permite crear enlaces cortos personalizados, rastrear clicks, y gestionar todos tus enlaces desde un dashboard intuitivo.',
+    link: 'https://cutfly.vercel.app',
+    github: 'https://github.com/omancillav/cutfly',
+    image: 'cutfly.webp',
+    tags: [TAGS.TYPESCRIPT, TAGS.NEXT, TAGS.TAILWIND, TAGS.SHADCN, TAGS.TURSO, TAGS.VERCEL]
+  },
+  {
+    title: 'Donly - Todo List App',
+    description:
+      'Donly es una aplicación de lista de tareas construida con HTML, CSS y JavaScript. Permite a los usuarios crear, editar, eliminar y marcar tareas como completadas de manera sencilla, con una interfaz intuitiva y responsiva.',
+    link: 'https://donly.vercel.app',
+    github: 'https://github.com/omancillav/donly',
+    image: 'donly.webp',
+    tags: [TAGS.HTML, TAGS.CSS, TAGS.JAVASCRIPT]
+  }
+]
