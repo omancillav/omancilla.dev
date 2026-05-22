e94807af07b12bcedc2618f3aa98dc6e2d1b4767
omancillav <edgarmncllav04@gmail.com>
2026-05-22 11:11:36 -0600

    feat: add Monitor icon and implement theme toggle synchronization; update experience section

--- src/lib/experience.ts
+++ src/lib/experience.ts
@@ -1,11 +1,11 @@
 export const EXPERIENCE = [
-  {
-    title: 'Desarrollador Jr - Vlim',
-    date: 'Mayo 2026 - Actualidad',
-    description:
-      'Implementación de sistemas basados en microservicios utilizando Moleculer, con patrones de comunicación asincrónica y orquestación de servicios, junto con desarrollo de dashboards e interfaces de usuario responsivas con React.',
-    link: 'https://www.vlim.com.mx/'
-  },
+  // {
+  //   title: 'Desarrollador Jr - Vlim',
+  //   date: 'Mayo 2026 - Actualidad',
+  //   description:
+  //     'Implementación de sistemas basados en microservicios utilizando Moleculer, con patrones de comunicación asincrónica y orquestación de servicios, junto con desarrollo de dashboards e interfaces de usuario responsivas con React.',
+  //   link: 'https://www.vlim.com.mx/'
+  // },
   {
     title: 'Desarrollador FullStack - WhatsBotSm',
     date: 'Septiembre 2024 - Abril 2026',
