0b69015a0f3838dd9259e272fff1dd85c34df9b3
omancillav <edgarmncllav04@gmail.com>
2026-07-12 20:05:23 -0600

    feat: reorganize technology stack

--- src/lib/stack.ts
+++ src/lib/stack.ts
@@ -8,8 +8,6 @@ import PHPIcon from '@/assets/Php.astro'
 import JavaScriptIcon from '@/assets/JavaScript.astro'
 import TypeScriptIcon from '@/assets/TypeScript.astro'
 import PythonIcon from '@/assets/Python.astro'
-import HTMLIcon from '@/assets/Html5.astro'
-import CSSIcon from '@/assets/CSS.astro'
 import ReactIcon from '@/assets/React.astro'
 import AstroIcon from '@/assets/AstroIcon.astro'
 import MySQLIcon from '@/assets/MySQL.astro'
@@ -18,53 +16,19 @@ import MongoDBIcon from '@/assets/Mongo.astro'
 import SQLiteIcon from '@/assets/SQLite.astro'
 import FirebaseIcon from '@/assets/Firebase.astro'
 import SupabaseIcon from '@/assets/Supabase.astro'
-import GitIcon from '@/assets/Git.astro'
 import GitHubIcon from '@/assets/GitHub.astro'
 import EslintIcon from '@/assets/Eslint.astro'
-import PostmanIcon from '@/assets/Postman.astro'
-import NpmIcon from '@/assets/Npm.astro'
 import VercelIcon from '@/assets/Vercel.astro'
-
-export const BACKEND = [
-  {
-    name: 'Node.js',
-    icon: NodeIcon
-  },
-  {
-    name: 'Express.js',
-    icon: ExpressIcon
-  },
-  {
-    name: 'Laravel',
-    icon: LaravelIcon
-  },
-  {
-    name: 'PHP',
-    icon: PHPIcon
-  },
-  {
-    name: 'Java',
-    icon: JavaIcon
-  },
-  {
-    name: 'Python',
-    icon: PythonIcon
-  }
-] as const
+import GcloudIcon from '@/assets/Gcloud.astro'
+import MoleculerIcon from '@/assets/Moleculer.astro'
+import ExpoIcon from '@/assets/Expo.astro'
+import TursoIcon from '@/assets/Turso.astro'
+import ZodIcon from '@/assets/Zod.astro'
+import CloudflareIcon from '@/assets/Cloudflare.astro'
+import JenkinsIcon from '@/assets/Jenkins.astro'
+import SQLIcon from '@/assets/SQL.astro'
 
 export const FRONTEND = [
-  {
-    name: 'HTML5',
-    icon: HTMLIcon
-  },
-  {
-    name: 'CSS3',
-    icon: CSSIcon
-  },
-  {
-    name: 'Tailwind CSS',
-    icon: TailwindIcon
-  },
   {
     name: 'JavaScript',
     icon: JavaScriptIcon
@@ -78,31 +42,74 @@ export const FRONTEND = [
     icon: ReactIcon
   },
   {
+    name: 'React Native',
+    icon: ReactIcon
+  },
+  {
     name: 'Next.js',
     icon: NextIcon
   },
   {
     name: 'Astro',
     icon: AstroIcon
+  },
+  {
+    name: 'Expo',
+    icon: ExpoIcon
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: TailwindIcon
+  }
+] as const
+
+export const BACKEND = [
+  {
+    name: 'Node.js',
+    icon: NodeIcon
+  },
+  {
+    name: 'Express',
+    icon: ExpressIcon
+  },
+  {
+    name: 'Moleculer',
+    icon: MoleculerIcon
+  },
+  {
+    name: 'Java',
+    icon: JavaIcon
+  },
+  {
+    name: 'Python',
+    icon: PythonIcon
+  },
+  {
+    name: 'PHP',
+    icon: PHPIcon
+  },
+  {
+    name: 'Laravel',
+    icon: LaravelIcon
+  },
+  {
+    name: 'Zod',
+    icon: ZodIcon
   }
 ] as const
 
 export const DATABASE = [
   {
-    name: 'MySQL',
-    icon: MySQLIcon
+    name: 'SQL',
+    icon: SQLIcon
   },
   {
     name: 'PostgreSQL',
     icon: PostgreSQLIcon
   },
   {
-    name: 'MongoDB',
-    icon: MongoDBIcon
-  },
-  {
-    name: 'SQLite',
-    icon: SQLiteIcon
+    name: 'MySQL',
+    icon: MySQLIcon
   },
   {
     name: 'Firebase',
@@ -111,32 +118,48 @@ export const DATABASE = [
   {
     name: 'Supabase',
     icon: SupabaseIcon
+  },
+  {
+    name: 'Turso/libSQL',
+    icon: TursoIcon
+  },
+  {
+    name: 'MongoDB',
+    icon: MongoDBIcon
+  },
+  {
+    name: 'SQLite',
+    icon: SQLiteIcon
   }
 ] as const
 
 export const TOOLS = [
   {
-    name: 'Git',
-    icon: GitIcon
-  },
-  {
-    name: 'GitHub',
-    icon: GitHubIcon
-  },
-  {
-    name: 'npm',
-    icon: NpmIcon
-  },
-  {
-    name: 'Eslint',
-    icon: EslintIcon
-  },
-  {
-    name: 'Postman',
-    icon: PostmanIcon
+    name: 'Google Cloud',
+    icon: GcloudIcon
   },
   {
     name: 'Vercel',
     icon: VercelIcon
+  },
+  {
+    name: 'Cloudflare',
+    icon: CloudflareIcon
+  },
+  {
+    name: 'EAS',
+    icon: ExpoIcon
+  },
+  {
+    name: 'Jenkins',
+    icon: JenkinsIcon
+  },
+  {
+    name: 'Git/GitHub',
+    icon: GitHubIcon
+  },
+  {
+    name: 'ESLint',
+    icon: EslintIcon
   }
 ] as const
